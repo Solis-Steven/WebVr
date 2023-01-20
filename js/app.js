10fed721c3ac927045c70d0c6c585fb88979a6bb
steven-solis <kp07761@gmail.com>
2023-01-20 13:06:52 -0600

    Escena terminada

--- js/app.js
+++ js/app.js
@@ -1,41 +1,35 @@
 import { sistema } from "./planetas.js";     
-
+const escena = document.querySelector("#escena");
 
 document.addEventListener('DOMContentLoaded', cargarPlanetas);
 
-
+// Escena de Steven
 function cargarPlanetas() {
-
-    // let entidadPadre = document.createElement("a-entity");
-    // let html = ``;
-    // sistema.forEach( planeta => {
-
-    //     html += `
-    //     <a-entity geometry="primitive: sphere; radius:25;"  
-    //               position="${planeta.posicionX} 0 -200" 
-    //               material="shader: flat; src: #${planeta.nombre};"
-    //               animation="property: rotation; dur: 3000; to: 0 360 0; easing: linear; loop: true">
-    //     </a-entity> 
-    //     `
-    // } );
-    // entidadPadre.innerHTML = html;
-    // entidadAnimacion.appendChild(entidadPadre);
-
-    const escena = document.querySelector("#escena")
 
     
     sistema.forEach( planeta => {
-        const entidadAnimacion = document.querySelector(`#${planeta.orbita}`)
+        /* Se crea una entidad y se agregan los atributos correspondientes
+        para que haya una animacion alrededor de la persona */
+        const entidadAnimacion = document.createElement("a-entity");
+        entidadAnimacion.setAttribute("id", planeta.orbita);
+        entidadAnimacion.object3D.position.set(0, 3, 0);
+        entidadAnimacion.object3D.rotation.set(0, 0, 0);
+        entidadAnimacion.setAttribute("animation", { property: "rotation", dur: `${planeta.duracion}`, to: "0 360 0", loop: true });
 
+        /* Se crea la entidad que corresponde a cada planeta y se agregan 
+        los atributos correspondientes para que aparezcan en la escena */
         let entidad = document.createElement("a-entity");
         entidad.setAttribute("geometry", { primitive: "sphere", radius: 2 });
         entidad.setAttribute("material", { shader: "flat", src: `#${planeta.nombre}` });
+        entidad.setAttribute("class", "planeta");
         entidad.setAttribute("animation", { property: "rotation", dur: "3000", to: "0 360 0", easing: "linear", loop: true });
         entidad.object3D.position.set( 0, 0, planeta.posicionZ);
         entidad.setAttribute("shootable", "")
         entidad.setAttribute("value", `${planeta.descripcion}`)
         
-        entidadAnimacion.appendChild( entidad );
+
+        entidadAnimacion.appendChild( entidad ); // Se pone el planeta a "orbitar"
+        escena.appendChild( entidadAnimacion ); // Se agrega el planeta a la escena
     } );
 }
 
--- js/planetas.js
+++ js/planetas.js
@@ -3,7 +3,8 @@ export const sistema = [
         nombre: "sol",
         posicionZ: -10,
         orbita: "solAnimacion",
-        descripcion: "Nuestra estrella es una entre las muchas que se pueden ver en el cielo nocturno. Se encarga de generar energía en su interior, la cual llega a la Tierra en solo 8 minutos. Sin embargo, en un futuro lejano, el sol agotará su combustible y liberará sus capas externas dando origen a una nebulosa planetaria, marcando el final de nuestra estrella. La estrella más cercana al Sol, Próxima Centauri, se encuentra a una distancia de 4,2 años luz, lo que nos muestra la vastedad del universo. Es importante destacar que las distancias en el Universo son enormes."
+        descripcion: "Nuestra estrella es una entre las muchas que se pueden ver en el cielo nocturno. Se encarga de generar energía en su interior, la cual llega a la Tierra en solo 8 minutos. Sin embargo, en un futuro lejano, el sol agotará su combustible y liberará sus capas externas dando origen a una nebulosa planetaria, marcando el final de nuestra estrella. La estrella más cercana al Sol, Próxima Centauri, se encuentra a una distancia de 4,2 años luz, lo que nos muestra la vastedad del universo.",
+        duracion: 10000
     },
 
     {
@@ -11,6 +12,7 @@ export const sistema = [
         posicionZ: -15,
         orbita: "mercurioAnimacion",
         descripcion: "Mercurio es el primer planeta rocoso y el más pequeño del Sistema Solar. A pesar de estar cerca al Sol, no es el más caliente debido a la falta de una atmósfera que regule su temperatura. Su superficie está altamente afectada por los impactos de asteroides y cometas, muchos de los cuales ocurrieron hace millones de años atrás. Como resultado, su superficie está cubierta de cráteres que permanecen hasta el día de hoy, similar a la Luna."
+        , duracion: 12500
     },
 
     {
@@ -18,6 +20,7 @@ export const sistema = [
         posicionZ: -20,
         orbita: "venusAnimacion",
         descripcion: "Después del Sol y la Luna, Venus es el objeto más radiante que podemos observar en el cielo. A pesar de tener un tamaño y una masa similares a la Tierra, su atmósfera, compuesta en gran medida por dióxido de carbono (CO2), es muy densa, causando un efecto invernadero tan extremo que incluso podría derretir el plomo. Durante mucho tiempo se creyó que debajo de las densas nubes se encontraba un clima tropical. Sin embargo, gracias a las observaciones en ondas de radio y a las misiones de sondeo, sabemos que la realidad es muy diferente. Debajo de las nubes, Venus es más bien un lugar similar al infierno."
+        , duracion: 15000
     },
 
     {
@@ -25,6 +28,7 @@ export const sistema = [
         posicionZ: -25,
         orbita: "tierraAnimacion",
         descripcion: "¡La Tierra es el lugar donde vivimos! Es nuestra casa y, hasta donde sabemos, el único planeta del Universo con vida. Su posición en el Sistema Solar es especial debido a que se encuentra a la distancia ideal para tener agua en estado líquido en su superficie. La Tierra tiene un satélite natural único, la Luna, hasta la fecha, el lugar más lejano alcanzado por un ser humano."
+        , duracion: 17500
     },
 
     {
@@ -32,6 +36,7 @@ export const sistema = [
         posicionZ: -30,
         orbita: "marteAnimacion",
         descripcion: "Marte es similar a la Tierra en muchos aspectos, como el duración del día, las estaciones y la presencia de agua bajo su superficie, lo que lo convierte en el mejor candidato para la colonización espacial. A pesar de esto, establecer una colonia en Marte no será una tarea fácil, ya que su atmósfera es muy tenue y necesitaremos trajes presurizados y oxígeno para poder caminar por su superficie. Además, la distancia entre Marte y la Tierra es 140 veces mayor que la distancia entre la Tierra y la Luna."
+        , duracion: 20000
     },
 
     {
@@ -39,6 +44,7 @@ export const sistema = [
         posicionZ: -35,
         orbita: "jupiterAnimacion",
         descripcion: "Te damos la bienvenida a Júpiter, el planeta más grande del Sistema Solar. Es masivo, dos veces y media más grande que todos los demás planetas combinados y tiene un volumen tan grande que podría albergar 1300 Tierras. Está compuesto principalmente por hidrógeno y helio, pero hacia el interior la presión aumenta y estos gases se vuelven líquidos. Una de las características de este gigante son las bandas claras y oscuras, que son sistemas de nubes a diferentes alturas. Lo más impresionante de Júpiter es la gran mancha roja, un huracán tan grande que podría contener dos Tierras y que lleva dando vueltas por más de 300 años."
+        , duracion: 22500
     },
 
     {
@@ -46,6 +52,7 @@ export const sistema = [
         posicionZ: -40,
         orbita: "saturnoAnimacion",
         descripcion: "Ahora te encuentras en Saturno, el segundo planeta más masivo del Sistema Solar, perteneciente a la categoría de planetas gaseosos junto con Júpiter, Urano y Neptuno, compuesto principalmente de hidrógeno y helio. Sin embargo, su característica más distintiva y impresionante es su sistema de anillos, conformado por innumerables partículas de roca y hielo con tamaños que van desde una casa hasta un grano de arena. Estos anillos se extienden por más de 300,000 km pero son increíblemente delgados, con un grosor de solo 0,001 mm si se compararan con una hoja de cuaderno."
+        , duracion: 25000
     },
 
     {
@@ -53,6 +60,7 @@ export const sistema = [
         posicionZ: -45,
         orbita: "uranoAnimacion",
         descripcion: "Urano se ubica a la doble distancia entre Saturno y el Sol. Es principalmente compuesto de hidrógeno y helio, pero también contiene trazas de metano, lo que le da su característico color azul-verdoso. Al igual que los otros planetas gaseosos del Sistema Solar (Júpiter, Saturno y Neptuno), Urano tiene huracanes y tormentas en su atmósfera, visibles como manchas claras o oscuras. Estas manchas duran varios meses o años antes de desaparecer. Urano fue descubierto accidentalmente en 1781 por Herschel mientras miraba a través de un telescopio."
+        , duracion: 27500
     },
 
     {
@@ -60,5 +68,6 @@ export const sistema = [
         posicionZ: -50,
         orbita: "neptunoAnimacion",
         descripcion: "Te encuentras en Neptuno, el último planeta del Sistema Solar. Es similar en tamaño y composición química a Urano, y su color azulado es debido al metano. Si continuaras avanzando, entrarías a la zona de cuerpos menores llamada cinturón de Kuiper, donde se encuentra Plutón, el planeta enano. Más allá está la nube de Oort, una colección de cuerpos helados que rodea todo el Sistema Solar y se extiende unos 2000 km. La estrella más cercana a nosotros, Próxima Centauri, se encuentra 4800 km más allá de la nube de Oort. La sonda Voyager 1, es el robot terrícola que ha llegado más lejos en la historia, y se encuentra a media hora de camino fuera del Sistema Solar."
+        , duracion: 30000
     },
 ]
