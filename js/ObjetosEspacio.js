3794a43f19d5e3dacbd1b0057c006924d9a90dc8
JoseRH25 <jlgamer25.ah@gmail.com>
2023-01-21 05:16:51 -0600

    Escena Jose

---
+++ js/ObjetosEspacio.js
@@ -0,0 +1,81 @@
+export const ObjetosEspaciales = [
+    {
+        nombre: "sol",
+        posicionx: 0,
+        posiciony: 0,
+        posicionZ: -40,
+        tamaño: 5,
+        rotacionx: 0,
+        rotaciony: 360,
+        rotacionz: 0, 
+        duracionRotacion: 3000,
+        orbita: "solAnimacion",
+        descripcion: "Estrella",
+        duracionOrbita: 10000
+    },
+
+    {
+        nombre: "meteorito",
+        posicionx: 0,
+        posiciony: 0,
+        posicionZ: 90,
+        tamaño: 2,
+        rotacionx: 0,
+        rotaciony: 360,
+        rotacionz: 0, 
+        duracionRotacion: 2000,
+        orbita: "meteoritoAnimacion",
+        descripcion: "Meteoroides:  Son fragmentos de cometas y asteroides, aunque también pueden ser rocas de satélites o planetas que han sido eyectadas en grandes impactos o simplemente restos de la formación de sistema solar.",
+        duracionOrbita: 20000
+    },
+
+    {
+        nombre: "oort",
+        posicionx: 0,
+        posiciony: 0,
+        posicionZ: 100,
+        tamaño: 8,
+        rotacionx: 0,
+        rotaciony: 360,
+        rotacionz: 0, 
+        duracionRotacion: 50000,
+        orbita: "oortAnimacion",
+        descripcion: "Nube de Oort: Es una nube de objetos transneptunianos que se encuentra en los límites del sistema solar, se encuentra 365 dias luz de distancia, la nube podría albergar entre uno y cien billones de objetos.",
+        duracionOrbita: 0
+    },
+
+    {
+        nombre: "asteroide",
+        posicionx: 0,
+        posiciony: 0,
+        posicionZ: 80,
+        tamaño: 4,
+        rotacionx: 0,
+        rotaciony: 360,
+        rotacionz: 0, 
+        duracionRotacion: 3000,
+        orbita: "asteroideAnimacion",
+        descripcion: "Asteroide:  es un cuerpo celeste acuoso, más pequeño que un planeta y mayor que un meteoroide. La mayoria orbita entre Marte y Júpiter, en la región del sistema solar conocida como cinturón de asteroides; otros se acumulan en los puntos de Lagrange de Jupiter, y la mayor parte del resto cruza las órbitas de los planetas.",
+        duracionOrbita: 18000
+    },
+
+    {
+        nombre: "agujeroNegro",
+        posicionx: 0,
+        posiciony: 50,
+        posicionZ: 0,
+        tamaño: 10,
+        rotacionx:0,
+        rotaciony: 360,
+        rotacionz: 0, 
+        duracionRotacion: 500,
+        orbita: "agujeroAnimacion",
+        descripcion: "Agujero Negro:  Son una región finita del espacio en cuyo interior posee una concentración de masa lo suficientemente elevada como para generar un campo gravitatorio, que provoca una singularidad envuelta por una superficie cerrada, llamada horizonte de sucesos. ",
+        duracionOrbita: 0
+    },
+
+
+    
+
+    
+]
