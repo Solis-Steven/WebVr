893aacb54400a22caf491ad3e63a23a364c1d45c
JoseRH25 <jlgamer25.ah@gmail.com>
2023-01-22 00:20:01 -0600

    EscenaTerminada

--- js/ObjetosEspacio.js
+++ js/ObjetosEspacio.js
@@ -4,20 +4,20 @@ export const ObjetosEspaciales = [
         posicionx: 0,
         posiciony: 0,
         posicionZ: -40,
-        tamaño: 5,
+        tamaño: 15,
         rotacionx: 0,
         rotaciony: 360,
         rotacionz: 0, 
-        duracionRotacion: 3000,
+        duracionRotacion: 7000,
         orbita: "solAnimacion",
-        descripcion: "Estrella",
+        descripcion: "Estrella:  son motores de energía cósmica que producen calor, luz, rayos ultravioleta, rayos X y otras formas de radiación. Están compuestas casi en su totalidad de gas y plasma, un estado de supercalentamiento de la materia compuesta de partículas subatómicas.  ",
         duracionOrbita: 10000
     },
 
     {
         nombre: "meteorito",
         posicionx: 0,
-        posiciony: 0,
+        posiciony: -80,
         posicionZ: 90,
         tamaño: 2,
         rotacionx: 0,
@@ -25,7 +25,7 @@ export const ObjetosEspaciales = [
         rotacionz: 0, 
         duracionRotacion: 2000,
         orbita: "meteoritoAnimacion",
-        descripcion: "Meteoroides:  Son fragmentos de cometas y asteroides, aunque también pueden ser rocas de satélites o planetas que han sido eyectadas en grandes impactos o simplemente restos de la formación de sistema solar.",
+        descripcion: "Meteoroides:  Son fragmentos de cometas y asteroides, aunque también pueden ser rocas de satlites o planetas que han sido eyectadas en grandes impactos o simplemente restos de la formación de sistema solar.",
         duracionOrbita: 20000
     },
 
@@ -46,8 +46,8 @@ export const ObjetosEspaciales = [
 
     {
         nombre: "asteroide",
-        posicionx: 0,
-        posiciony: 0,
+        posicionx: 20,
+        posiciony: -20,
         posicionZ: 80,
         tamaño: 4,
         rotacionx: 0,
@@ -55,7 +55,7 @@ export const ObjetosEspaciales = [
         rotacionz: 0, 
         duracionRotacion: 3000,
         orbita: "asteroideAnimacion",
-        descripcion: "Asteroide:  es un cuerpo celeste acuoso, más pequeño que un planeta y mayor que un meteoroide. La mayoria orbita entre Marte y Júpiter, en la región del sistema solar conocida como cinturón de asteroides; otros se acumulan en los puntos de Lagrange de Jupiter, y la mayor parte del resto cruza las órbitas de los planetas.",
+        descripcion: "Asteroide:  Es un cuerpo celeste acuoso, más pequeño que un planeta y mayor que un meteoroide. La mayoria orbita entre Marte y Júpiter, en la región del sistema solar conocida como cinturón de asteroides; otros se acumulan en los puntos de Lagrange de Jupiter, y la mayor parte del resto cruza las órbitas de los planetas.",
         duracionOrbita: 18000
     },
 
@@ -64,7 +64,7 @@ export const ObjetosEspaciales = [
         posicionx: 0,
         posiciony: 50,
         posicionZ: 0,
-        tamaño: 10,
+        tamaño: 20,
         rotacionx:0,
         rotaciony: 360,
         rotacionz: 0, 
@@ -73,6 +73,99 @@ export const ObjetosEspaciales = [
         descripcion: "Agujero Negro:  Son una región finita del espacio en cuyo interior posee una concentración de masa lo suficientemente elevada como para generar un campo gravitatorio, que provoca una singularidad envuelta por una superficie cerrada, llamada horizonte de sucesos. ",
         duracionOrbita: 0
     },
+
+    {
+        nombre: "satelite",
+        posicionx: 0,
+        posiciony: -40,
+        posicionZ: -20,
+        tamaño: 2,
+        rotacionx:0,
+        rotaciony: 0,
+        rotacionz: 360, 
+        duracionRotacion: 5000,
+        orbita: "SateliteAnimacion",
+        descripcion: "La Luna es el único satélite natural de la Tierra. Se encuentra a 384 400 km de distancia de la Tierra y tiene un diámetro de 3476 km. Tarda en dar una vuelta alrededor de la Tierra unos 27.32 días. Debido a efectos de marea, el periodo de rotación de su eje coincide con el de traslación en torno a la Tierra, por lo que la Luna siempre presenta la misma cara al observador terrestre.    ",
+        duracionOrbita: 10000
+    },
+
+    {
+        nombre: "nebulosa",
+        posicionx: 35,
+        posiciony: -15,
+        posicionZ: 60,
+        tamaño: 5,
+        rotacionx:0,
+        rotaciony: 0,
+        rotacionz: 360, 
+        duracionRotacion: 15000,
+        orbita: "nebulosaAnimacion",
+        descripcion: "Nebulosa Oscuras: Son nubes de gas y polvo de baja temperatura que no emiten luz visible y ocultan las estrellas que se encuentran en su interior o detrás de ellas.  ",
+        duracionOrbita: 20000
+    },
+
+    {
+        nombre: "nubeInterestelar",
+        posicionx: 0,
+        posiciony: 30,
+        posicionZ: 45,
+        tamaño: 5,
+        rotacionx:0,
+        rotaciony: 0,
+        rotacionz: 360, 
+        duracionRotacion: 10000,
+        orbita: "nubeAnimacion",
+        descripcion: "Nube Interestelar: Son parches de polvo y gas densos en el espacio, generalmente 10.000 veces mas voluminosos que el Sol, de los cuales se cree que se forman las estrellas. Las particulas de la nube son atraidas entre si, lo que ocasiona que formen cuerpos giratorios más pequeños, que pueden convertirse en estrellas.   ",
+        duracionOrbita: 25000
+    },
+
+    
+    {
+        nombre: "polvoCosmico",
+        posicionx: 0,
+        posiciony: 25,
+        posicionZ: 35,
+        tamaño: 5,
+        rotacionx:0,
+        rotaciony: 0,
+        rotacionz: 360, 
+        duracionRotacion: 10000,
+        orbita: "polvoAnimacion",
+        descripcion: "Nube Interestelar: Son granos de materia que se mueven en torno a los planetas o a las estrellas, concentrados en nubes, anillos o cinturones.  ",
+        duracionOrbita: 12000
+    },
+
+    {
+        nombre: "planetaTierra",
+        posicionx: 0,
+        posiciony: -40,
+        posicionZ: 0,
+        tamaño: 10,
+        rotacionx:0,
+        rotaciony: 360,
+        rotacionz: 0, 
+        duracionRotacion: 10000,
+        orbita: "planetaAnimacion",
+        descripcion: "Planeta: es un cuerpo celeste que orbita alrededor del Sol, posee suficiente masa como para que su propia gravedad domine las fuerzas presentes como cuerpo rígido, lo que implica una forma aproximadamente redondeada determinada por el equilibrio hidrostático, es el objeto claramente dominante en su vecindad, habiendo limpiado su órbita de cuerpos similares a él.   ",
+        duracionOrbita: 8000
+    },
+
+    {
+        nombre: "vacio",
+        posicionx: 50,
+        posiciony: -30,
+        posicionZ: -50,
+        tamaño: 3,
+        rotacionx:360,
+        rotaciony: 360,
+        rotacionz: 0, 
+        duracionRotacion: 5000,
+        orbita: "vacioAnimacion",
+        descripcion: "Vacio:  Son los espacios entre filamentos, la estructura de mayor escala en el universo, que contiene muy pocas o ninguna galaxia.",
+        duracionOrbita: 10000
+    },
+
+
 
 
     
